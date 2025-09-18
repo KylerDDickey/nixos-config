ce384734a68e623cd358677455446f7059addb78
KylerDDickey <kyler.dwayne.dickey@gmail.com>
2025-09-18 10:54:42 -0500

    Update building steps

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
           #   maintainers = with lib.maintainers; [  ];
           # };
 
-          builder = ./scripts/build.sh;
+          builder = ./scripts/builder.sh;
         });
       });
     };
