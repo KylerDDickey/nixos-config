5b96921bb5243660fad7c78f329b52dc236dece1
KylerDDickey <kyler.dwayne.dickey@gmail.com>
2025-09-28 09:38:32 -0500

    Update IDE script

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -22,27 +22,33 @@
   home.packages = with pkgs; [
     (writeShellApplication {
       name = "ide";
-      runtimeInputs = [ tmux ];
+      runtimeInputs = [
+        tmux
+        util-linux
+      ];
       text = ''
         NUM_COLS=$(tput cols)
         NUM_LINES=$(tput lines)
         CMD_WINDOW_PERCENT_SIZE=20%
 
+        SESSION_ID=$(uuidgen)
+        SESSION_NAME=dev-$SESSION_ID
+
         # Start a new detached tmux session named 'dev', ensure window is sized the same
         # as the current terminal.
-        tmux new-session -s "dev" -d -x "$NUM_COLS" -y "$NUM_LINES"
+        tmux new-session -s "$SESSION_NAME" -d -x "$NUM_COLS" -y "$NUM_LINES"
 
         # Split the window vertically, make sure the new pane is only N% of the window.
-        tmux split-window -t dev -v -l "$CMD_WINDOW_PERCENT_SIZE"
+        tmux split-window -t $SESSION_NAME -v -l "$CMD_WINDOW_PERCENT_SIZE"
 
         # Move cursor back to the nvim terminal.
-        tmux last-pane -t dev
+        tmux last-pane -t $SESSION_NAME
 
         # Send the nvim command to the first pane of the 'dev' session.
-        tmux send-keys -t dev "nvim" C-m
+        tmux send-keys -t $SESSION_NAME "nvim" C-m
 
         # Attach to the tmux session.
-        tmux attach-session -t dev
+        tmux attach-session -t $SESSION_NAME
       '';
     })
   ];
